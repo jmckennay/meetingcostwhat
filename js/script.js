655ed7e062577bb14c5eb7295c5eb1422d056796
James McKennay <james.mckennay@tattsgroup.com>
2019-11-08 15:24:02 +1000

    Restore from cookie

--- js/script.js
+++ js/script.js
@@ -18,10 +18,25 @@ $(window).resize(function() {
     viewportWidth = $(window).width();
 });
 
+if(getCookie('attendees')){
+    $('[name="attendees"]').val(getCookie('attendees'));
+}
+
+if(getCookie('rate')){
+    $('[name="rate"]').val(getCookie('rate'));
+}
+
+
 calculateCost();
 
 var loop;
-var started;
+var started = getCookie('started');
+
+if(started!== null){
+ var temp = new Date(0);
+ started = temp.setUTCMilliseconds(started);   
+ start(true);
+}
 
 moment.updateLocale('en', {
     relativeTime : {
@@ -37,6 +52,7 @@ $('button').on('click tap', function() {
 
 function addTime(time) {
     started = started - time;
+    setCookie('started',started,30);
 }
 
 function toggleDark() {
@@ -55,8 +71,15 @@ function start(resume){
     $('.adjustments').css('max-height', '40px');
     counter.css('transform', 'scale(1)')
     clearInterval(loop);
-    started = Date.now();
+    if(!resume){
+        started = Date.now();
+    }
     calculateCost();
+
+    setCookie('started',started,30);
+    setCookie('rate',$('[name="rate"]').val(), 30);
+    setCookie('attendees',$('[name="attendees"]').val(), 30);
+
     let lastTimerUpdate = Date.now();
     timer.html('for ' + moment(started).fromNow());
     
@@ -91,8 +114,40 @@ function start(resume){
 
 function stop(){
     clearInterval(loop);
+    eraseCookie('started');
+    eraseCookie('rate');
+    eraseCookie('attendees');
     $('#start, [name="attendees"], [name="rate"]').removeAttr("disabled");
     $('#stop').attr("disabled", true);
     $('.settings').css('max-height', '');
     $('.adjustments').css('max-height', '');
+}
+
+function reset() {
+    stop();
+    $('[name="attendees"]').val(10);
+    $('[name="rate"]').val(100);
+}
+
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+function eraseCookie(name) {   
+    document.cookie = name+'=; Max-Age=-99999999;';  
 }
